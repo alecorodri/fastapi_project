6a01ee5667a94dc3ee52684ac750887257cfd227
aleco <66926752+alecorodri@users.noreply.github.com>
2022-11-22 03:14:19 -0500

    add  User Model

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 from fastapi import FastAPI
 import uvicorn
 from pydantic import BaseModel
-
+from typing import Optional
+from datetime import datetime
 
 app =  FastAPI(title='Proyecto para reseñar peliculas',
     description='En este proyecto seremos capaces de reseñar peliculas',
     version='1.0.0')
+
+
+
+#User Model
+class User(BaseModel): #schemas
+    id: int
+    name: str
+    lastName: str
+    direction: Optional[str]
+    telephone: int
+    userCreation: datetime = datetime.now()
+
+@app.post('/ruta2')
+def ruta2(user:User):
+    user = user.dict()
+    print(user)
+    return True
+
 
 @app.on_event('startup')
 def startUp():
@@ -14,11 +33,6 @@ def startUp():
 @app.on_event('shutdown')
 def shutDown():
     print('Finalizando server')
-
-@app.post('/ruta2')
-def ruta2(user):
-    print(user)
-    return True
 
 @app.get('/')
 async def index():
