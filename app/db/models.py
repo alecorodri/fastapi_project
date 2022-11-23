729cfee51426a858c27b0fa2b45ba9a7de526fc7
aleco <66926752+alecorodri@users.noreply.github.com>
2022-11-23 00:29:36 -0500

    add sale model

--- app/db/models.py
+++ app/db/models.py
@@ -1,11 +1,15 @@
 from app.db.db import Base
 from sqlalchemy import Column,Integer,String,Boolean,DateTime
 from datetime import datetime
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class User(Base):
     __tablename__= "user"
     id = Column(Integer,primary_key=True,autoincrement=True)
+    userName = Column(String)
+    password = Column(String)
     name = Column(String)
     lastName = Column(String)
     address = Column(String)
@@ -13,3 +17,11 @@ class User(Base):
     email = Column(String)
     creation = Column(DateTime,default=datetime.now,onupdate=datetime.now)
     status = Column(Boolean)
+    sale = relationship("sale",backref="user",cascade="delete,merge")
+
+class Sale(Base):
+    __tablename__= "sale"
+    id = Column(Integer,primary_key=True,autoincrement=True)
+    userId = Column(Integer,ForeignKey("user.id",ondelete="CASCADE"))
+    sale = Column(Integer)
+    productSale = Column(Integer)
